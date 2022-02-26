2e6f870b9ac3567bd2830760de25751b6183ffbc
Gustavo Rosa <gustavorib.rosa@gmail.com>
2022-02-26 20:10:24 -0300

    delete button added to posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -20,6 +20,14 @@ class PostsController < ApplicationController
         end
     end
 
+    def destroy
+        @post = Post.find(params[:id])
+
+        @post.destroy
+
+        redirect_to root_url
+    end
+
     private
     def post_params
         params.require(:post).permit(:body)
